cb9bc0b614acc8b6bba8ac49b5d1e22059e18e91
techno_us <ruslan.tihonovusow@gmail.com>
2020-11-05 12:43:09 +0300

    JS one todo left

--- js/main-js.js
+++ js/main-js.js
@@ -1,32 +1,31 @@
-const headerMenu = document.querySelector(".header-menu")
+const headerMenuBackground = document.querySelector(".header-menu-background")
 
 window.addEventListener('scroll', function (e) {
     const offsetFromTop = document.querySelector('html').scrollTop;
     if (offsetFromTop > 0) {
-        headerMenu.style.background = 'linear-gradient(0deg, rgba(255,255,255,1) 22%, rgba(231,183,183,1) 30%, rgba(186,0,0,1) 63%, rgba(255,255,255,1) 78%)';
+        headerMenuBackground.style.opacity = '1';
     } else {
-        headerMenu.style.background = 'none';
+        headerMenuBackground.style.opacity = '0';
     }
 })
 
-document.querySelectorAll('.video-button-shadow')[0].addEventListener('click', function (e) {
+document.querySelector('.video-button-shadow').addEventListener('click', function (e) {
     document.querySelector('.container-video-block').style.display = 'none';
-    document.querySelector('#rabbitVideo').play();
+    document.querySelector('#videoRabbit').play();
 });
 
 const buttons = document.querySelectorAll('.statistic-button');
-const offers = document.querySelectorAll('.offer-line');
+const offers = document.querySelectorAll('.offer');
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function (event) {
+    buttons[i].onclick = function (event) { //TODO replace onclick with addEventListener
         for (let j = 0; j < buttons.length; j++) {
             buttons[j].classList.remove('statistic-button_active');
             offers[j].classList.remove('offer_active');
         }
-        const choosedPlan = event.target.getAttribute('.statistic-button');
-        console.log('choosedPlan', choosedPlan)
+        const choosedPlan = event.target.getAttribute('data-offerName');
         const choosedClass = `${choosedPlan}`;
         const choosedElement = document.querySelector(choosedClass);
-        choosedElement.classList.add('.offer_active');
+        choosedElement.classList.add('offer_active');
         this.classList.add('statistic-button_active');
     };
 }
